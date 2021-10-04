b1b8810433f6e2f4fe85fa4df19a701287c245ac
marcos <marcostkao@gmail.com>
2021-10-03 22:01:52 -0300

    feat: add item component

--- src/app/product-detail/item/item.component.ts
+++ src/app/product-detail/item/item.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { Item } from './item.model';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.scss']
+  styleUrls: ['./item.component.scss'],
 })
 export class ItemComponent implements OnInit {
+  @Input() item: Item;
+  @Output() add = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  emitAddEvent() {
+    this.add.emit(this.item);
   }
-
 }
