a7dcec611c0c674e5747eb80efd03d92e8c5f8c9
marcos <marcostkao@gmail.com>
2021-10-03 22:00:25 -0300

    chore: filter category product-detail

--- src/app/product-detail/product-detail-routing.module.ts
+++ src/app/product-detail/product-detail-routing.module.ts
@@ -1,11 +1,23 @@
 import { ProductDetailComponent } from './product-detail.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ItemComponent } from './item/item.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: ProductDetailComponent
+    component: ProductDetailComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'item',
+        pathMatch: 'full'
+      },
+      {
+        path: 'item',
+        component: ItemComponent
+      }
+    ]
   }
 ];
 
--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,15 +1,35 @@
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Category } from './../categories/category';
+import { CategoriesService } from './../categories/categories.service';
+import { Item } from './item/item.model';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.scss']
+  styleUrls: ['./product-detail.component.scss'],
 })
 export class ProductDetailComponent implements OnInit {
+  category: Category;
 
-  constructor() { }
+  items: Observable<Item[]>
+
+  constructor(
+    private categoriesService: CategoriesService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.categoriesService
+      .categoryById(this.route.snapshot.params['id'])
+      .subscribe((category) => (this.category = category));
+
+   this.items = this.categoriesService.itemOfCategories(this.route.parent.snapshot.params['id'])
   }
 
+  addItem(item: Item){
+    console.log(item);
+  }
 }
